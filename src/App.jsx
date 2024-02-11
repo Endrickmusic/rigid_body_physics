650b9a5460aaee3a540cc1b3ffd9d39366a6c64f
Christian <mail@christianhohenbild.de>
2024-02-11 14:58:34 +0100

    add colors, switch spheres for boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { useRef, useReducer, useMemo } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useReducer, useMemo, useEffect } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Environment, Lightformer, OrbitControls } from '@react-three/drei'
 import { Vector3, MathUtils } from 'three'
-import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
+import { CuboidCollider, Physics, RigidBody } from '@react-three/rapier'
 import { easing } from 'maath'
 
 import './index.css'
 
-const accents = ['#ff4060', '#ffcc00', '#20ffa0', '#4060ff']
+const accents = ['#ff0000', '#00ff00', '#0000ff', '#aa00ff', '#00ffff', '#ffff00', '#999999']
 const shuffle = (accent = 0) => [
-  { color: '#444', roughness: 0.1, metalness: 0.5 },
-  { color: '#444', roughness: 0.1, metalness: 0.5 },
-  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.9 },
+  { color: '#444', roughness: 0.1, metalness: 0.9 },
+  { color: '#444', roughness: 0.1, metalness: 0.9 },
   { color: 'white', roughness: 0.1, metalness: 0.1 },
   { color: 'white', roughness: 0.1, metalness: 0.1 },
   { color: 'white', roughness: 0.1, metalness: 0.1 },
   { color: accents[accent], roughness: 0.1, accent: true },
   { color: accents[accent], roughness: 0.1, accent: true },
   { color: accents[accent], roughness: 0.1, accent: true },
-  { color: '#444', roughness: 0.1 },
-  { color: '#444', roughness: 0.3 },
-  { color: '#444', roughness: 0.3 },
-  { color: 'white', roughness: 0.1 },
-  { color: 'white', roughness: 0.2 },
-  { color: 'white', roughness: 0.1 },
+  { color: '#222', roughness: 0.1 },
+  { color: '#222', roughness: 0.3 },
+  { color: '#222', roughness: 0.3 },
+  { color: '#ff00aa', roughness: 0.1 },
+  { color: '#ff00aa', roughness: 0.2 },
+  { color: '#ff00aa', roughness: 0.1 },
   { color: accents[accent], roughness: 0.1, accent: true, transparent: true, opacity: 0.5 },
   { color: accents[accent], roughness: 0.3, accent: true },
   { color: accents[accent], roughness: 0.1, accent: true }
@@ -32,6 +32,7 @@ const shuffle = (accent = 0) => [
 export default function App(props) {
   const [accent, click] = useReducer((state) => ++state % accents.length, 0)
   const connectors = useMemo(() => shuffle(accent), [accent])
+
   return (
   <>
 
@@ -40,19 +41,28 @@ export default function App(props) {
   shadows 
   onClick={click} 
   dpr={[1, 1.5]} 
-  gl={{ antialias: false }} 
-  camera={{ position: [0, 0, 30], fov: 17.5, near: 10, far: 40 }} 
+  gl={{ antialias: true }} 
+  camera={{ position: [0, 0, 30], 
+    fov: 17.5, 
+    near: 1, 
+    far: 1000 }} 
   {...props}>
 
       <color attach="background" args={[0x999999]} />
 
       <OrbitControls />
 
-      <Physics /*debug*/ timeStep="vary" gravity={[0, 0, 0]}>
-      {connectors.map((props, i) => (
-          <Sphere key={i} {...props} />
+      <Physics 
+        timeStep="vary" 
+        gravity={[0, 0, 0]}>
+        {connectors.map((props, i) => (
+        
+        <Sphere 
+        key={i} 
+        {...props} />
         ))}
       </Physics>
+      
       <Environment resolution={256}>
         <group rotation={[-Math.PI / 3, 0, 1]}>
           <Lightformer form="circle" intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} />
@@ -68,22 +78,31 @@ export default function App(props) {
 }
 
 function Sphere({ position, children, vec = new Vector3(), scale, r = MathUtils.randFloatSpread, accent, color = 'white', ...props }) {
+  
   const api = useRef()
   const ref = useRef()
   const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+  
   useFrame((state, delta) => {
     delta = Math.min(0.1, delta)
     api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
     easing.dampC(ref.current.material.color, color, 0.2, delta)
   })
+  
   return (
-    <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
-      <BallCollider args={[1]} />
+    <RigidBody 
+      linearDamping={4} 
+      angularDamping={1} 
+      friction={0.1} 
+      position={pos} 
+      ref={api} 
+      colliders={false}>
+      <CuboidCollider args={[1, 1, 1]} />
       <mesh 
       ref={ref} 
       castShadow 
       receiveShadow>
-        <sphereGeometry args={[1, 64, 64]} />
+        <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial {...props} />
         {children}
       </mesh>
