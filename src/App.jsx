9fec28fae95e98ac257752d40097f7bd4ff04e29
Christian <mail@christianhohenbild.de>
2024-02-11 01:39:32 +0100

    add objects, lights and physics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,92 @@
-import { Canvas } from '@react-three/fiber'
+import { useRef, useReducer, useMemo } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Environment, Lightformer, OrbitControls } from '@react-three/drei'
+import { Vector3, MathUtils } from 'three'
+import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
+import { easing } from 'maath'
 
 import './index.css'
-import Shader from './Shader.jsx'
 
-function App() {
-  
+const accents = ['#ff4060', '#ffcc00', '#20ffa0', '#4060ff']
+const shuffle = (accent = 0) => [
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: '#444', roughness: 0.1 },
+  { color: '#444', roughness: 0.3 },
+  { color: '#444', roughness: 0.3 },
+  { color: 'white', roughness: 0.1 },
+  { color: 'white', roughness: 0.2 },
+  { color: 'white', roughness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true, transparent: true, opacity: 0.5 },
+  { color: accents[accent], roughness: 0.3, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true }
+]
+
+export default function App(props) {
+  const [accent, click] = useReducer((state) => ++state % accents.length, 0)
+  const connectors = useMemo(() => shuffle(accent), [accent])
   return (
   <>
 
-    <Canvas
-    camera={{ 
-      position: [0, 0, 2],
-      fov: 40 }}  
-    >
+  <Canvas 
+  flat 
+  shadows 
+  onClick={click} 
+  dpr={[1, 1.5]} 
+  gl={{ antialias: false }} 
+  camera={{ position: [0, 0, 30], fov: 17.5, near: 10, far: 40 }} 
+  {...props}>
+
       <color attach="background" args={[0x999999]} />
-      <Shader />
+
+      <OrbitControls />
+
+      <Physics /*debug*/ timeStep="vary" gravity={[0, 0, 0]}>
+      {connectors.map((props, i) => (
+          <Sphere key={i} {...props} />
+        ))}
+      </Physics>
+      <Environment resolution={256}>
+        <group rotation={[-Math.PI / 3, 0, 1]}>
+          <Lightformer form="circle" intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} />
+          <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={2} />
+          <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={2} />
+          <Lightformer form="circle" intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={8} />
+          <Lightformer form="ring" color="#4060ff" intensity={80} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 10, 0]} scale={10} />
+        </group>
+      </Environment>
     </Canvas>
   </>
   )
 }
 
-export default App
+function Sphere({ position, children, vec = new Vector3(), scale, r = MathUtils.randFloatSpread, accent, color = 'white', ...props }) {
+  const api = useRef()
+  const ref = useRef()
+  const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+  useFrame((state, delta) => {
+    delta = Math.min(0.1, delta)
+    api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
+    easing.dampC(ref.current.material.color, color, 0.2, delta)
+  })
+  return (
+    <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
+      <BallCollider args={[1]} />
+      <mesh 
+      ref={ref} 
+      castShadow 
+      receiveShadow>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial {...props} />
+        {children}
+      </mesh>
+    </RigidBody>
+  )
+}
